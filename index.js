f6c74f3f2ae87dac601016e2e25f6d6e60bec647
piuswong-multiverse <111539998+piuswong-multiverse@users.noreply.github.com>
2022-12-07 11:14:31 -0600

    Express server runs

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // App Server code goes here
+// Display animals when you visit website
+// display different animals depending on the url visited
+
+const express = require('express');
+const app = express();
+const port = 3001;
+
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`)
+})
